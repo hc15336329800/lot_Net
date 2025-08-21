492bfea6589aabc8d77e463eb90e22df2201114f
zgr <zhangguorenmi@163.com>
2025-08-21 08:53:16 +0800

    1

---
+++ RuoYi.Iot/mysql/sys_job.sql
@@ -0,0 +1,41 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : ry_net_sass
+
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
+
+ Date: 21/08/2025 08:52:17
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_job
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_job`;
+CREATE TABLE `sys_job`  (
+  `job_id` bigint(20) NOT NULL COMMENT '任务ID',
+  `job_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '任务名称',
+  `job_group` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'DEFAULT' COMMENT '任务组名',
+  `invoke_target` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '调用目标字符串',
+  `cron_expression` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT 'cron执行表达式',
+  `misfire_policy` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '3' COMMENT '计划执行错误策略（1立即执行 2执行一次 3放弃执行）',
+  `concurrent` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '1' COMMENT '是否并发执行（0允许 1禁止）',
+  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '状态（0正常 1暂停）',
+  `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '创建者',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '更新者',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '备注信息',
+  PRIMARY KEY (`job_id`, `job_name`, `job_group`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 104 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '定时任务调度表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- RuoYi.Iot/mysql/sys_job_iot.sql
+++ RuoYi.Iot/mysql/sys_job_iot.sql
@@ -1,19 +1,41 @@
-﻿DROP TABLE IF EXISTS sys_job_iot;
-CREATE TABLE sys_job_iot (
-  job_id        bigint(20)    NOT NULL COMMENT '任务ID',
-  target_type   varchar(64)   DEFAULT '' COMMENT '目标类型',
-  task_type     varchar(64)   DEFAULT '' COMMENT '任务类型',
-  device_id     bigint(20)    DEFAULT NULL COMMENT '设备ID',
-  product_id     bigint(20)    DEFAULT NULL COMMENT '所属产品ID',
+/*
+ Navicat Premium Data Transfer
 
-  select_points varchar(500)  DEFAULT '' COMMENT '选择点位（ （ iot_product_point表的 point_key 字段））',
-  trigger_source varchar(64)  DEFAULT '' COMMENT '触发源',
-  status        char(1)       DEFAULT '0' COMMENT '状态',
-  create_by     varchar(64)   DEFAULT '' COMMENT '创建者',
-  create_time   datetime      DEFAULT NULL COMMENT '创建时间',
-  update_by     varchar(64)   DEFAULT '' COMMENT '更新者',
-  update_time   datetime      DEFAULT NULL COMMENT '更新时间',
-  remark        varchar(500)  DEFAULT '' COMMENT '备注',
-  PRIMARY KEY (job_id),
-  CONSTRAINT fk_sys_job_iot_job FOREIGN KEY (job_id) REFERENCES sys_job(job_id)
-) ENGINE=InnoDB COMMENT='定时任务IOT扩展表';
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80013
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : ry_net_sass
+
+ Target Server Type    : MySQL
+ Target Server Version : 80013
+ File Encoding         : 65001
+
+ Date: 21/08/2025 08:52:13
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for sys_job_iot
+-- ----------------------------
+DROP TABLE IF EXISTS `sys_job_iot`;
+CREATE TABLE `sys_job_iot`  (
+  `job_id` bigint(20) NOT NULL COMMENT '任务ID',
+  `target_type` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '目标类型',
+  `task_type` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '任务类型',
+  `product_id` bigint(20) NULL DEFAULT NULL COMMENT '所属产品ID',
+  `device_id` bigint(20) NULL DEFAULT NULL COMMENT '设备ID',
+  `select_points` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '选择点位（ 产品表的 point_key）',
+  `trigger_source` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '触发源',
+  `status` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '状态',
+  `create_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '创建者',
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '更新者',
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`job_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '定时任务IOT扩展表' ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
