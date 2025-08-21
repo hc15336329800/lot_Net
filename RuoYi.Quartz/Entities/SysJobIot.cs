5cad50cff4882873a404268ff31f757686b3b1db
zgr <zhangguorenmi@163.com>
2025-08-21 18:00:19 +0800

    1

--- RuoYi.Quartz/Entities/SysJobIot.cs
+++ RuoYi.Quartz/Entities/SysJobIot.cs
@@ -12,7 +12,7 @@ namespace RuoYi.Quartz.Entities
         /// <summary>
         /// 任务ID
         /// </summary>
-        [SugarColumn(IsPrimaryKey = true,ColumnName = "job_id",ColumnDescription = "任务ID")]
+        [SugarColumn(ColumnName = "job_id",ColumnDescription = "任务ID",IsPrimaryKey = true)]
         public long JobId { get; set; }
 
         /// <summary>
